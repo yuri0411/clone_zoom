7af2ec3c26c73b92c98270a7944b93f749f73893
yuri <yu_ri0411@naver.com>
2024-08-21 12:13:19 +0900

    #1.2
    - install ws
    - wss 설정

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from 'http'
+import { WebSocketServer } from "ws";
 import express from 'express';
 
 const app = express();
@@ -8,5 +10,11 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http: `);
-app.listen(3000, handleListen)
+const handleListen = () => console.log(`Listening on ws://localhost:3000 `);
+
+// http 서버애 access
+const server = http.createServer(app)
+// http 서버 위에 webSocket 서버를 만듬.
+const wss = new WebSocketServer({server});
+
+server.listen(3000, handleListen);
