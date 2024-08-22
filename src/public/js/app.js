11ce60b3fc204ee99381176bbc7b510cc0566792
yuri <yu_ri0411@naver.com>
2024-08-22 14:06:31 +0900

    #1.7 nickname 추가

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,7 +1,13 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 // 서버로의 연결
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 // 서버 연결 떄
 socket.addEventListener("open", (e) => {
@@ -10,7 +16,9 @@ socket.addEventListener("open", (e) => {
 
 // 서버에서 메세지를 받을 떼
 socket.addEventListener("message", (message) => {
-  console.log(`New message: ${message.data}`);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 // 서버와 연결이 끊겼을 때
@@ -18,9 +26,17 @@ socket.addEventListener("close", () => {
   console.log("Disconnected from Server");
 });
 
-messageForm.addEventListener("submit", (e) => {
+function handleSubmit(e) {
   e.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
   input.value = "";
-});
+}
+
+function handleNickSubmit(e) {
+  e.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+}
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
